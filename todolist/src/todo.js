ecd636886581e0048aad3de58d92c852dc41628b
Wangzongcan <wang.zongcan@gmail.com>
2015-07-28 15:01:53 +0800

    重新修改结构 可点击完成任务

--- todolist/src/todo.js
+++ todolist/src/todo.js
@@ -1,9 +1,66 @@
-var AddInput = React.createClass({
+var ToDoItem = React.createClass({
+  getInitialState: function() {
+    return {done: false};
+  },
+
+  handleClick: function() {
+    if (this.state.done) {
+      this.setState({done: false});
+    } else {
+      this.setState({done: true});
+    }
+  },
+
+  render: function() {
+    var liStyle;
+
+    if (this.state.done) {
+      liStyle = {
+        textDecoration: 'line-through'
+      };
+    }
+
+    return (
+        <li style={liStyle} key={this.props.i} onClick={this.handleClick}>{this.props.value}</li>
+    );
+  }
+});
+
+var ToDoList = React.createClass({
+  getInitialState: function(data) {
+    return {data: data};
+  },
+
+  render: function() {
+    var toDoList = this.props.data.map(function (item, i){
+      return (
+        <ToDoItem value={item.text} i={i} />
+      );
+    });
+
+    return (
+      <ul>
+        {toDoList}
+      </ul>
+    );
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+
   handleSubmit: function(e) {
     if (e.which == 13) {
       var text = React.findDOMNode(this.refs.text);
 
-      $("#list").append("<li>"+text.value.trim()+"</li>");
+      if (text.value == "") {return;}
+
+      data = this.state.data;
+      data.push({text: text.value});
+
+      this.setState({data: data});
 
       text.value = "";
     }
@@ -11,21 +68,13 @@ var AddInput = React.createClass({
 
   render: function() {
     return (
-      <input type="text" ref="text" onKeyPress={this.handleSubmit} />
-    );
-  }
-});
-
-var ToDoList = React.createClass({
-  render: function() {
-    return (
       <div className="list">
         <h1>ToDoList</h1>
-        <ul id="list"></ul>
-        <AddInput />
+        <ToDoList data={this.state.data} />
+        <input type="text" ref="text" onKeyPress={this.handleSubmit} />
       </div>
     );
   }
 });
 
-React.render(<ToDoList />, $("body")[0]);
+React.render(<App />, document.body);
